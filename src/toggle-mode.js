cafca09ed7af10efc353c5562bd8369d86d69e4a
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-01-25 18:54:16 -0300

    translate page to english

--- src/toggle-mode.js
+++ src/toggle-mode.js
@@ -6,6 +6,6 @@ buttonToggle.addEventListener('click', (event) => {
   const mode = darkMode ? 'light' : 'dark'
   event.currentTarget.querySelector(
     'span'
-  ).textContent = `${mode} mode ativado.`
+  ).textContent = `${mode} mode activated.`
   darkMode = !darkMode
 })
